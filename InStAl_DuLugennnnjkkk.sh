bd026e4af593f929e318ef9f3a30c015f79a5d92
prajurit-pajat <117523991+prajurit-pajat@users.noreply.github.com>
2022-12-18 19:09:29 +0700

    Add files via upload

---
+++ InStAl_DuLugennnnjkkk.sh
@@ -0,0 +1,157 @@
+
+# code warna
+c='\e[0;36m' # biru muda
+b='\e[0;33m' # kuning
+lg='\e[1;32m' # hijau
+w='\e[1;37m' # putih
+clear
+echo -e $lg
+echo "  _____      _  _______ ____  "
+echo " | ____|    | |/ /_   _|  _ \ "
+echo " |  _| _____| ' /  | | | |_) |"
+echo " | |__|_____| . \  | | |  __/ "
+echo " |_____|    |_|\_\ |_| |_|    "
+echo -e $b"——————————————————————————————————————————————————"
+echo -e $lg"{"$c"•"$lg"}"$w" Author"$c" :"$w" trio renkarnasi"
+echo -e $lg"{"$c"•"$lg"}"$w" Team  "$c" :"$w" mafia teknologi"
+echo -e $b"——————————————————————————————————————————————————"
+echo
+echo -e $lg"   {"$c"•"$lg"}"$w" 10 sample data perekaman "$lg"e-ktp"$w" 2012"$lg" {"$c"•"$lg"}"
+echo -e $c"+"$lg"=================="$b"×××××××××"$lg"===================="$c"+"
+echo -e $b" ["$c"01"$b"]"$lg"."$w" data sample e-ktp"
+echo -e $b" ["$c"02"$b"]"$lg"."$w" beli data sample e-ktp : 1357 data"
+echo -e $b" ["$c"03"$b"]"$lg"."$w" order script"
+echo -e $b" ["$c"04"$b"]"$lg"."$w" join to grup"
+echo -e $c"+"$lg"=================="$b"×××××××××"$lg"===================="$c"+"
+echo -e $w
+read -p "choose > " chos;
+if [[ $chos == "1" ]] || [[ $chos == "01" ]]; then
+sleep 1
+echo "[1]. SUHEDI 03-MARET-1970"
+   echo "=================================================="
+   echo "{•} nama    : suhedi"
+   echo "{•} klamin  : laki-laki"
+   echo "{•} no kk   : 3303150203055143"
+   echo "{•} no nik  : 3303150303700002"
+   echo "{•} alamat  : purbayasa rt 1 / rw 1"
+   echo "{•} tanggal : 25-05-2012"
+   echo "=================================================="
+sleep 1
+echo "[2]. FAJAR EKO PURNOMO 15-DESEMBER-1993"
+   echo "=================================================="
+   echo "{•} nama    : fajar eko purnomo"
+   echo "{•} klamin  : laki-laki"
+   echo "{•} no kk   : 3303150203055143"
+   echo "{•} no nik  : 3303151512930002"
+   echo "{•} alamat  : purbayasa rt 1 / rw 1"
+   echo "{•} tanggal : 25-05-2012"
+   echo "=================================================="
+sleep 1
+echo "[3]. ALFIAH 16-SEPTEMBER-1975"
+   echo "=================================================="
+   echo "{•} nama    : alfiah"
+   echo "{•} klamin  : perempuan"
+   echo "{•} no kk   : 3303150203055143"
+   echo "{•} no nik  : 3303156609750002"
+   echo "{•} alamat  : purbayasa rt 1 / rw 1"
+   echo "{•} tanggal : 02-06-2012"
+   echo "=================================================="
+sleep 1
+echo "[4]. RUSWANI 15-SEPTEMBER-1944"
+   echo "=================================================="
+   echo "{•} nama    : ruswani"
+   echo "{•} klamin  : laki-laki"
+   echo "{•} no kk   : 3303150203055784"
+   echo "{•} no nik  : 3303151509440001"
+   echo "{•} alamat  : purbayasa rt 1 / rw 1"
+   echo "{•} tanggal : 25-05-2012"
+   echo "=================================================="
+sleep 1
+echo "[5]. TURIAH 10-DESEMBER-1946"
+   echo "=================================================="
+   echo "{•} nama    : turiah"
+   echo "{•} klamin  : perempuan"
+   echo "{•} no kk   : 3303150203055784"
+   echo "{•} no nik  : 3303155012460002"
+   echo "{•} alamat  : purbayasa rt 1 / rw 2"
+   echo "{•} tanggal : 25-05-2012"
+   echo "=================================================="
+sleep 1
+echo "[6]. ADI SUTARDI 11-APRIL-1968"
+   echo "=================================================="
+   echo "{•} nama    : adi sutardi"
+   echo "{•} klamin  : laki-laki"
+   echo "{•} no kk   : 3303150203055785"
+   echo "{•} no nik  : 3303151104680001"
+   echo "{•} alamat  : purbayasa rt 1 / rw 1"
+   echo "{•} tanggal : 25-05-2012"
+   echo "=================================================="
+sleep 1
+echo "[7]. WATIAH 15-JUNI-1977"
+   echo "=================================================="
+   echo "{•} nama    : watiah"
+   echo "{•} klamin  : perempuan"
+   echo "{•} no kk   : 3303150203055785"
+   echo "{•} no nik  : 3303155406770002"
+   echo "{•} alamat  : purbayasa rt 1 / rw 1"
+   echo "{•} tanggal : 25-05-2012"
+   echo "=================================================="
+sleep 1
+echo "[8]. S PARTOHARJO 07-MEI-1933"
+   echo "=================================================="
+   echo "{•} nama    : s partoharjo"
+   echo "{•} klamin  : laki-laki"
+   echo "{•} no kk   : 3303150203055786"
+   echo "{•} no nik  : 3303150705330001"
+   echo "{•} alamat  : purbayasa rt 1 / rw 1"
+   echo "{•} tanggal : 02-06-2012"
+   echo "=================================================="
+sleep 1
+echo "[9]. HARYATI 10-FEBRUARI-1945"
+   echo "=================================================="
+   echo "{•} nama    : haryati"
+   echo "{•} klamin  : perempuan"
+   echo "{•} no kk   : 3303150203055786"
+   echo "{•} no nik  : 3303155002450002"
+   echo "{•} alamat  : purbayasa rt 1 / rw 2"
+   echo "{•} tanggal : 25-05-2012"
+   echo "=================================================="
+sleep 1
+echo "[10]. SUDIONO 02-FEBRUARI-1958"
+   echo "=================================================="
+   echo "{•} nama    : sudiono"
+   echo "{•} klamin  : laki-laki"
+   echo "{•} no kk   : 3303150203055787"
+   echo "{•} no nik  : 3303150202580001"
+   echo "{•} alamat  : purbayasa rt 1 / rw 1"
+   echo "{•} tanggal : 25-05-2012"
+   echo "=================================================="
+elif [[ $chos == "2" ]] || [[ $chos == "02" ]]; then
+echo
+echo "{•} ORDER DATA PEREKAMAN E-KTP "
+echo "====================================================="
+echo "{•} harga    : rp 500.000"
+echo "{•} file     : pdf"
+echo "{•} jumlah   : 1357 data"
+echo "{•} isi data : nama,gender,nik,kk,alamat,tgl rekam"
+echo "{•} transaksi: dana / rekening"
+echo "====================================================="
+echo "[y] beli  |  [n] tidak"
+echo "====================================================="
+read -p "[y/n] >>>> " yn;
+   if [[ $yn == "y" ]] || [[ $yn == "Y" ]]; then
+     xdg-open
+   else
+       exit
+   fi
+elif [[ $chos == "3" ]] || [[ $chos == "03" ]]; then
+   echo
+   echo "{•} order     : script codingan"
+   echo "{•} bahasa    : bash - python"
+   echo "{•} harga     : 50k - 500k"
+   echo "{•} owner     : +62 857-7936-8705"
+   echo "{•} transaksi : dana/rekening"
+   echo "=========================================="
+elif [[ $chos == "4" ]] || [[ $chos == "03" ]]; then
+xdg-open https://chat.whatsapp.com/IxAmrmNN5Kk2S75iUtNQkh
+fi
